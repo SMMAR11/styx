5ab79994fdc3d354138f5136fe1264ef34e90a7f
chakibBH <cbenhabib@neogeo.fr>
2018-08-28 15:21:02 +0200

    fix realisation_etat/avancement_programme encodage:cp1252 + ordre colonnes

--- app/views/avancement_programme.py
+++ app/views/avancement_programme.py
@@ -11,9 +11,9 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
 import csv
-import json
 
 decorators = [csrf_exempt, login_required(login_url='index')]
+
 
 @method_decorator(decorators, name='dispatch')
 class AvancementProgrammeView(View):
@@ -27,35 +27,6 @@ class AvancementProgrammeView(View):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.columns = [
-            'id', 'id_progr_id', 'intitule_programme', 'id_org_moa_id',
-            'numero_axe', 'intitule_axe', 'numero_sous_axe', 'intitule_sous_axe',
-            'montant_autres_accorde', 'montant_adour_garonne_accorde',
-            'montant_cd09_en_dde_versement', 'montant_europe_axe',
-            'montant_cd31_verse', 'montant_etat_en_dde_versement',
-            'montant_autres_verse', 'montant_cd09_accorde', 'montant_edf_accorde',
-            'part_moa_axe', 'montant_edf_en_dde_versement',
-            'part_etat_axe', 'part_cd31_axe', 'montant_region_accorde',
-            'montant_aeag_en_dde_versement', 'part_cd34_axe', 'montant_europe_accorde',
-            'montant_adour_garonne_axe', 'montant_aermc_verse', 'montant_cd31_axe',
-            'montant_aermc_axe', 'montant_autres_axe', 'montant_cd66_axe',
-            'montant_edf_axe', 'montant_etat_verse', 'part_autres_axe',
-            'montant_cd11_en_dde_versement', 'montant_cd11_accorde', 'montant_facture',
-            'montant_region_en_dde_versement', 'montant_europe_verse', 'annee_prev_echeance',
-            'montant_axe', 'montant_etat_axe', 'part_cd66_axe', 'part_edf_axe',
-            'montant_region_verse', 'montant_cd34_accorde', 'montant_cd31_accorde',
-            'montant_cd34_axe', 'montant_europe_en_dde_versement', 'part_aermc_axe',
-            'axe_ttc', 'montant_cd31_en_dde_versement', 'part_cd09_axe', 'montant_supplementaire',
-            'montant_region_axe', 'montant_dossiers', 'maitre_ouvrage', 'montant_moa_axe',
-            'montant_autres_en_dde_versement', 'part_cd11_axe', 'montant_aermc_en_dde_versement',
-            'montant_cd11_verse', 'montant_aermc_accorde', 'montant_edf_verse',
-            'montant_cd09_axe', 'montant_cd09_verse', 'montant_cd34_verse',
-            'montant_etat_accorde', 'part_region_axe', 'montant_commande_prestataire',
-            'montant_cd66_en_dde_versement', 'montant_cd34_en_dde_versement',
-            'montant_cd66_accorde', 'part_europe_axe', 'montant_cd11_axe',
-            'montant_cd66_verse', 'part_adour_garonne_axe', 'nombre_dossier',
-            'montant_adour_garonne_verse']
-        self.columns_sql = ", ".join(self.columns)
 
     def list_fetch_all(self, cursor):
         columns = [col[0] for col in cursor.description]
@@ -77,14 +48,14 @@ class AvancementProgrammeView(View):
         Les donnée sont ordonnées selon la variable self.columns
         """
 
-        sql = """SELECT {columns} FROM public.v_progs_detailles_complet;
-        """.format(columns=self.columns_sql)
+        sql = """SELECT * FROM public.v_progs_detailles_complet;
+        """
         return self.fetch_raw_data(sql)
 
     def download_csv(self, data):
-        response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition']="attachment; filename={}.csv".format(gen_cdc())
-        writer = csv.DictWriter(response, self.columns, delimiter=';')
+        response = HttpResponse(content_type='text/csv', charset='cp1252')
+        response['Content-Disposition'] = "attachment; filename={}.csv".format(gen_cdc())
+        writer = csv.DictWriter(response, data[0].keys(), delimiter=';')
         writer.writeheader()
         writer.writerows(data)
         return response
