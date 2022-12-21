b7e80f12d684e65abcfa6055d153ab3f8e2f4d12
MAYSONOVE Alexis <smmareptb11@gmail.com>
2022-12-21 16:45:40 +0100

    Annulation reverse proxy

--- styx/settings.py
+++ styx/settings.py
@@ -121,25 +121,11 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-reverseProxyRoot = config('REVERSEPROXY_ROOT', default=None)
-
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-staticUrl = '/'
-if reverseProxyRoot:
-    staticUrl += reverseProxyRoot + '/'
-staticUrl += 'static/'
-STATIC_URL = staticUrl
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-mediaUrl = '/'
-if reverseProxyRoot:
-    mediaUrl += reverseProxyRoot + '/'
-mediaUrl += 'media/'
-MEDIA_URL = mediaUrl
+MEDIA_URL = '/media/'
 
 LEAFLET_CONFIG = {
     'DEFAULT_CENTER' : (43.20, 2.32),
